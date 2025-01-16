9038b2be3fe57a2bfb011291f25cca8aecdc7b44
Confusified <60018584+Confusified@users.noreply.github.com>
2025-01-16 04:51:05 +0100

    merge rework into master (#30)
    * create new project for killbind mod
    
    * couple of compatibility stuff, workin on the rest
    
    * kill bind is back in action
    
    * tidying up

---
+++ Kill_Bind/Hooks/DependencyRelated/ToilHeadCompatibility.cs
@@ -0,0 +1,44 @@
+using System;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using com.github.zehsteam.ToilHead;
+using GameNetcodeStuff;
+
+namespace Kill_Bind.Hooks.DependencyRelated;
+
+public class ToilHead
+{
+    public static bool ToilHeadMod_Present = false;
+    // This value is responsible for spawning a Toil-Head OR a Slayer-Player (?), depending on RNG.
+    public static float ToilSlayerRagdollChance = 10f; // Default value of ToilHead is 10, so I replicate the same default
+    internal static ConfigEntry<float> ToilPlayerSlayerChance = null!;
+
+    internal static void CreateToilheadRagdoll(PlayerControllerB self)
+    {
+        bool editRagdoll = Utils.RandomPercent(ToilSlayerRagdollChance);
+        TurretHeadManager.SetDeadBodyTurretHead(self, editRagdoll);
+    }
+    private static void Activate()
+    {
+        ToilHeadMod_Present = true;
+        ConfigFile config = Chainloader.PluginInfos["com.github.zehsteam.ToilHead"].Instance.Config;
+        foreach (ConfigDefinition key in config.Keys)
+        {
+            if (key.Section == "Toil-Player Settings" && key.Key == "ToilPlayerSlayerChance")
+            {
+                config.TryGetEntry<float>(key, out ToilPlayerSlayerChance);
+                ToilSlayerRagdollChance = ToilPlayerSlayerChance.Value;
+                Main.Logger.LogDebug($"Updated ToilSlayerRagdollChance to {ToilSlayerRagdollChance}");
+                ToilPlayerSlayerChance.SettingChanged += UpdateChanceValue;
+                return;
+            }
+        }
+        Main.Logger.LogDebug($"Could not find ToilPlayerSlayerChance config setting. ToilSlayerRagdollChance will use the default value set by KillBind ({ToilSlayerRagdollChance})");
+    }
+
+    private static void UpdateChanceValue(object sender = null!, EventArgs args = null!)
+    {
+        ToilSlayerRagdollChance = ToilPlayerSlayerChance.Value;
+        Main.Logger.LogDebug($"Updated ToilSlayerRagdollChance to {ToilSlayerRagdollChance}");
+    }
+}
---
+++ Kill_Bind/Main.cs
@@ -0,0 +1,116 @@
+using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Kill_Bind.Config;
+using Kill_Bind.Hooks;
+using Kill_Bind.Hooks.DependencyRelated;
+using LethalCompanyInputUtils.Api;
+using LethalCompanyInputUtils.BindingPathEnums;
+using UnityEngine.InputSystem;
+
+namespace Kill_Bind;
+public class KillBind_Inputs : LcInputActions
+{
+    [InputAction(KeyboardControl.Backspace, Name = "Suicide", ActionType = InputActionType.Button)]
+    public InputAction ActionKillBind { get; set; }
+}
+
+// Soft Dependencies
+[SoftDependency("BMX.LobbyCompatibility", typeof(RegisterPlugin_LobbyCompatibility))]
+[SoftDependency("com.github.zehsteam.ToilHead", typeof(ToilHead))]
+// While LethalConfig is not necessary, it is highly recommended.
+[SoftDependency("ainavt.lc.lethalconfig", typeof(SetupConfig_LethalConfig))]
+
+// Hard Dependencies
+[BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.HardDependency)]
+
+// Plugin
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Main : BaseUnityPlugin
+{
+    public static Main Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+    private static readonly string configLocation = Utility.CombinePaths(Paths.ConfigPath + "\\" + MyPluginInfo.PLUGIN_GUID[9..21].Replace(".", "\\")) + MyPluginInfo.PLUGIN_GUID[..8];
+    internal static ConfigFile killbindConfig = new(configLocation + ".cfg", false);
+    public static readonly KillBind_Inputs InputActionInstance = new();
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        ConfigHandler.InitialiseConfig();
+        // Running code from the soft dependencies in the same file will cause an error if you do not have the dependency.
+        InitialiseSoftDependencies();
+        HookMethods();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    private static void InitialiseSoftDependencies()
+    {
+        Logger.LogDebug("Activating Soft Dependencies...");
+
+        SoftDependencyAttribute.Init(Instance);
+
+        Logger.LogDebug("Soft Dependencies Activated.");
+    }
+
+    private static void HookMethods()
+    {
+        Logger.LogDebug("Hooking...");
+
+        /*
+         *  Subscribe with 'On.Class.Method += CustomClass.CustomMethod;' for each method you're patching.
+         */
+        On.StartOfRound.Start += StartOfRoundHooks.UpdateRagdollTypeList;
+        Logger.LogDebug("Hooked: StartOfRound, Start");
+
+        InputActionInstance.ActionKillBind.performed += KillBindHandler.OnPressKillBind;
+        Logger.LogDebug("Bound KillBind's Keybind.");
+
+        Logger.LogDebug("Finished Hooking.");
+    }
+}
+
+// Source: https://discord.com/channels/1168655651455639582/1216761387343151134/1216761387343151134, Lethal Company Modding, Discord Server
+internal class SoftDependencyAttribute : BepInDependency
+{
+    public System.Type Handler;
+
+    /// <summary>
+    /// Marks this BepInEx.BaseUnityPlugin as soft depenant on another plugin.
+    /// The handler type must have an Initialize() method that will automatically be invoked if the compatible dependency is present.
+    /// </summary>
+    /// <param name="guid">The GUID of the referenced plugin.</param>
+    /// <param name="handlerType">The class type that will handle this compatibility. Must contain a private method called Initialize()</param>
+    public SoftDependencyAttribute(string guid, System.Type handlerType) : base(guid, DependencyFlags.SoftDependency)
+    {
+        Handler = handlerType;
+    }
+
+    /// <summary>
+    /// Global initializer for this class.
+    /// You must call this method from your base plugin Awake method and pass the plugin instance to the source parameter.
+    /// </summary>
+    /// <param name="source">The source plugin instance with the BepInPlugin attribute.</param>
+    internal static void Init(BaseUnityPlugin source)
+    {
+        const BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
+
+        IEnumerable<SoftDependencyAttribute> attributes = source.GetType().GetCustomAttributes<SoftDependencyAttribute>();
+        foreach (SoftDependencyAttribute attr in attributes)
+        {
+            if (attr == null) continue;
+            if (Chainloader.PluginInfos.ContainsKey(attr.DependencyGUID))
+            {
+                Main.Logger.LogDebug("Found compatible mod: " + attr.DependencyGUID);
+                attr.Handler.GetMethod("Activate", bindingFlags)?.Invoke(null, null);
+                attr.Handler = null!;
+            }
+        }
+    }
+}
